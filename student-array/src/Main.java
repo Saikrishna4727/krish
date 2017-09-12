2db3a0eb6392d8e1d575ffbac92b20bc20276534
Saikrishna4727 <31858968+Saikrishna4727@users.noreply.github.com>
2017-09-12 13:54:49 +0530

    two

---
+++ student-array/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Student s1=new Student();
+		s1.Student();
+		s1. getId();
+		s1.setId(int id);
+		s1.String getFullName();
+		s1.setFullName(String fullName);
+		s1.Date getBirthDate();
+		s1.setBirthDate(Date birthDate);
+		s1.getAvgMark();
+		s1.setAvgMark(double avgMark);
+		s1.hashCode();
+		s1. equals(Object obj);
+		s1.compareTo(Object o);
+		
+		
+	}
+
+}
