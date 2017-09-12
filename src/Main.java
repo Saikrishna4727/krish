0a6041337b32c1fd65d85de32a5996d0fbd650ca
Saikrishna4727 <31858968+Saikrishna4727@users.noreply.github.com>
2017-09-12 13:09:05 +0530

    my first

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		student k=new student();
+		k.student();
+		k.getId();
+		k.setId();
+		k.String getFullName();
+		k.setFullName(String fullName);
+		k. Date getBirthDate();
+		k.setBirthDate(Date birthDate);
+		k.getAvgMark();
+		k.setAvgMark(double avgMark);
+		k.hashCode();
+		
+	}
+
+}
